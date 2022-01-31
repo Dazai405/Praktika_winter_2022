98d781964f724ecd047e2d7a4ac558bf80c64b51
hezebr <90828493+hezebr@users.noreply.github.com>
2022-02-01 02:39:49 +0300

    Add files via upload

---
+++ src/main/java/com/example/navigation/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // получаем navController
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navFragment) as NavHostFragment? ?: return
+        val navController = host.navController
+
+        // включаем боковое меню
+        val sideBar = findViewById<NavigationView>(R.id.nav_view)
+        sideBar?.setupWithNavController(navController)
+
+        // настраиваем и включаем панель инструментов (toolbar)
+        val appBarConfiguration = AppBarConfiguration(navController.graph
+            , drawerLayout = drawer_layout) // для бокового меню
+        val toolBar = findViewById<Toolbar>(R.id.toolbar)
+        toolBar.setupWithNavController(navController, appBarConfiguration)
+    }
+}
